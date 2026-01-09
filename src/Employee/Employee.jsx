d2a929a7e5776fcd72d2aa0002c41078958a891c
FefeCodes <feranmiaina17@gmail.com>
2026-01-09 16:14:23 +0100

    added more files

---
+++ src/Employee/Employee.jsx
@@ -0,0 +1,13 @@
+import SideBar from "../SideBar";
+import TopContentCompt from "../TopContentCompt";
+
+export default function Employee() {
+  return (
+    <div>
+      <SideBar />
+      <div>
+        <TopContentCompt />
+      </div>
+    </div>
+  );
+}
